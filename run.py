f4b2f1343ba056e73ad49c7bef2de2eb30d22d3e
Crespo21 <tshepolesley21@gmail.com>
2025-09-28 20:00:21 +0200

    fix: Update database and requirements for compatibility; change host to localhost in run.py

--- run.py
+++ run.py
@@ -14,4 +14,4 @@ from src.main import app
 if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("run:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("run:app", host="127.0.0.1", port=port, reload=True)
